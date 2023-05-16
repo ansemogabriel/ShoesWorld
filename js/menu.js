c2212094787c3ee4d26e0828d14332d70afa890e
ansemogabriel <ansemogabriel@gmail.com>
2023-05-15 21:44:06 -0300

    save rev 9

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,20 @@
-/* Arquivos das automações do Menu Hamburger */
+/* Arquivos das automações do Menu Hamburger */ 
+
+const btnMobile = document.getElementById('btn-mobile');
+// Declarando a variavel com o CONST
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
